3b369dc0892aa7e46a6662a67a8a56071ca24f07
chaoscarla <carla.huber@stud.edubs.ch>
2025-10-20 15:53:26 +0200

    steppp

---
+++ DasProjekt/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(20, 0, 0);
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
+    private PlayerController playerControllerScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject. Find("Player One").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
+    }
+}
---
+++ DasProjekt/linear.py
@@ -0,0 +1,13 @@
+Liste = [5,3,7,6,8,9,0,2,1,4]
+
+number = input("Gebe eine Der Zehn Ziffern, nach der du Suchen möchtest:")
+
+def search_number(number):
+    for n in range (0,10):
+        if nuber == Liste (n):
+            print ("Zahl " + nummber + " an Stelle " + n + " gefunden.")
+
+        else:
+            continue
+
+search_number(number)
